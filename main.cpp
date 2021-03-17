86e2ac6fe147b97fb037ab4249dddbf717b81b40
~orda~ <Orda04@gmail.com>
2021-03-17 22:06:10 +0800

    rm Task2.pro and Task2.pro.user

--- main.cpp
+++ main.cpp
@@ -9,11 +9,11 @@
 
 
 //DONE● Создать и инициализировать переменные пройденных типов данных (short int, int, long long, char, bool, float, double).
-//DONE● Создать перечисление (enum) с возможными вариантами символов для игры в крестики-нолики.  LINE 24
-//DONE● Создать массив, способный содержать значения такого перечисления и инициализировать его.  LINE 45
+//DONE● Создать перечисление (enum) с возможными вариантами символов для игры в крестики-нолики.  LINE 21
+//DONE● Создать массив, способный содержать значения такого перечисления и инициализировать его.  LINE 42
 //DONE● * Создать структуру (struct) данных «Поле для игры в крестики-нолики» и снабдить его всеми необходимыми свойствами (подумайте что может понадобиться). Probably will add something else later
 //DONE● ** Создать структуру (struct) объединяющую: union (int, float, char) и 3-и битовых поля (флага) указывающими какого типа значение в данный момент содержится в объединении (isInt, isFloat, isChar).
-//Продемонстрировать пример использования в коде этой структуры.
+//DONE● Продемонстрировать пример использования в коде этой структуры.
 
 
 #include<iostream>
@@ -40,15 +40,14 @@ enum Rows   //List of field Rows
 
 };
 struct TicTacField
-
 {
- char VisualField[3][3]{{Nothing,Nothing,Nothing},{Nothing,Nothing,Nothing},{Nothing,Nothing,Nothing}} ; // Massive for visualisation on screen will be used at visualisation function later
- char Pl1Mark='X';                //    player 1 will have possibility to choose mark for use |default is X
- char Pl2MArk='O';                //    player 2 will have possibility to choose mark for use |default is O
- char Nothing=' ';                //    You can fill all field with different background marks|defauld =SPASE
- Conditions Field[3][3]={{EMPTY,EMPTY,EMPTY},{EMPTY,EMPTY,EMPTY},{EMPTY,EMPTY,EMPTY}};  //massive what our field is Originally is 0 everywhere
-int GameStatus=0;                 //0-In progress  1-is finised / ready to start
-int WhoWinner=0;                  //0 nobody win, 1 First player,2 Second Player
+ char visualField[3][3]{{nothing,nothing,nothing},{nothing,nothing,nothing},{nothing,nothing,nothing}} ; // Massive for visualisation on screen will be used at visualisation function later
+ char pl1Mark='X';                //    player 1 will have possibility to choose mark for use |default is X
+ char pl2MArk='O';                //    player 2 will have possibility to choose mark for use |default is O
+ char nothing=' ';                //    You can fill all field with different background marks|defauld =SPASE
+ Conditions  field[3][3]={{EMPTY,EMPTY,EMPTY},{EMPTY,EMPTY,EMPTY},{EMPTY,EMPTY,EMPTY}};  //massive what our field is Originally is 0 everywhere
+int gameStatus=0;                 //0-In progress  1-is finised / ready to start
+int whoWinner=0;                  //0 parity , 1 First player win,2 Second Player win
 };
 
 //Task 5
@@ -56,9 +55,9 @@ struct SmThg
 {
    union
    {
-       int Integer;
-       float Float;
-       char Char[2];
+       int integerData;
+       float floatData;
+       char charData[2];
    }Type;
 
    bool isInt=false;
@@ -71,32 +70,32 @@ struct SmThg
 
 int main()
 {
- short int SInt=32767;                                    //short int   2 bytes
- int Int=5;                                               //integer     4 bytes
- long long  LLong=9223372036854775807;                    //Long long   8 bytes
- char Char=126;    //Could be  char Char='~';             //Char        1 byte
- bool Bool=true;                                          //Boolean     Could be true(1) or false (o)
- float Float=456.9987;                                    //float       4 bytes   REAL VALUE
- double Double(5e-2);                                     //double      8 bytes   REAL VALUE
+ short int sIntValue=32767;                                    //short int   2 bytes
+ int intValue=5;                                               //integer     4 bytes
+ long long  llongValue=9223372036854775807;                    //Long long   8 bytes
+ char charValue=126;    //Could be  char Char='~';             //Char        1 byte
+ bool boolValue=true;                                          //Boolean     Could be true(1) or false (o)
+ float floatValue=456.9987;                                    //float       4 bytes   REAL VALUE
+ double doubleValue(5e-2);                                     //double      8 bytes   REAL VALUE
  int i=0,j=0;
-  cout<<"short int="<<SInt<<endl;
-   cout<<"int="<<Int<<endl;
-    cout<<"longlong="<<LLong<<endl;
-     cout<<"char="<<Char<<endl;
-      cout<<"bool="<<Bool<<endl;
-       cout<<"float="<<Float<<endl;
-        cout<<"double="<<Double<<endl;
+  cout<<"short int="<<sIntValue<<endl;
+   cout<<"int="<<intValue<<endl;
+    cout<<"longlong="<<llongValue<<endl;
+     cout<<"char="<<charValue<<endl;
+      cout<<"bool="<<boolValue<<endl;
+       cout<<"float="<<floatValue<<endl;
+        cout<<"double="<<doubleValue<<endl;
 
 //*************************************************************************************************
 
-TicTacField ProcessField;
+TicTacField processField;
 
 for(i=0;i<=2;i++)
 {
   for (j=0;j<=2;j++)
    {
-      ProcessField.Field[i][j]=XPLAYER;                  //Checking for appropriate changing parameters in TicTacToe field  you can put only 0,1,2 what we have on enum Conditions
-      cout<<" "<< ProcessField.Field[i][j];
+      processField. field[i][j]=XPLAYER;                  //Checking for appropriate changing parameters in TicTacToe field  you can put only 0,1,2 what we have on enum Conditions
+      cout<<" "<< processField.field[i][j];
   }
     cout<<endl;
 }
@@ -104,16 +103,16 @@ for(i=0;i<=2;i++)
     struct SmThg  ValueInside;
     cout<<"Please enter something integer\n";
 
-    cin>>ValueInside.Type.Integer;
+    cin>>ValueInside.Type.integerData;
     ValueInside.isInt=true;
 
 
     if(ValueInside.isInt)
     {
-     cout<<"Your values :\n"<< ValueInside.Type.Integer<<"\n";
+     cout<<"Your values :\n"<< ValueInside.Type.integerData<<"\n";
     }
      int x;     //костыль, в случае вылетающего терминала нужен чтобы не перенастраивать другие среды.
-             cin>>x;
+      cin>>x;
 
     return 0;
 }
